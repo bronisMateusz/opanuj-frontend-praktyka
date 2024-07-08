2428b2683ea21f9409ab983fa76aea3099b6f999
bronisMateusz <mateusz.bronis99@gmail.com>
2024-07-08 23:27:52 +0200

    feat: Add Comments component for app-performance

---
+++ practice/module1/lesson5/app-performance/src/components/Comments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import { Comment } from '../types/Comment';
+
+import CommentsForm from './CommentsForm.vue';
+import Placeholder from './Placeholder.vue';
+
+const comments = inject('comments') as Comment[];
+</script>
+
+<template>
+  <h2 class="font-bold text-xl my-2">Comments</h2>
+  <div class="grid gap-2">
+    <Placeholder v-if="!comments.length" :lines="5" :height="16" />
+    <div
+      v-else
+      v-for="comment in comments"
+      :key="comment.id"
+      class="bg-violet-400 rounded-xl text-white p-4 shadow"
+    >
+      <p class="italic">{{ comment.text }}</p>
+      <p class="text-sm text-violet-900">
+        {{ comment.author }} - Rating {{ comment.rating }}/5
+      </p>
+    </div>
+    <CommentsForm />
+  </div>
+</template>
