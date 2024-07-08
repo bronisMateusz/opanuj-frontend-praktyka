407b64bb966139bcf9bac8227c4d9e6f678bc9e9
bronisMateusz <mateusz.bronis99@gmail.com>
2024-07-08 23:28:13 +0200

    refactor: Update page title in index.html and remove unnecessary code in App.vue

--- practice/module1/lesson5/app-performance/src/App.vue
+++ practice/module1/lesson5/app-performance/src/App.vue
@@ -1,30 +1,69 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent, provide, ref, watch } from 'vue';
+import { useQuery } from '@tanstack/vue-query';
+import axios from 'axios';
+
+import { Comment } from './types/Comment';
+
+import Articles from './components/Articles.vue';
+import Authors from './components/Authors.vue';
+import Comments from './components/Comments.vue';
+
+const fetchData = async (url: string) => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const { data: authorsData } = useQuery({
+  queryKey: ['authors'],
+  queryFn: () =>
+    fetchData('http://localhost:3000/api/data/authors?timeout=5000').then(
+      (data) => data.authors
+    ),
+});
+
+const { isPending: isArticlesPending, data: articlesData } = useQuery({
+  queryKey: ['articles'],
+  queryFn: () =>
+    fetchData('http://localhost:3000/api/data/articles?timeout=3000').then(
+      (data) => data.articles
+    ),
+});
+
+const { data: commentsData } = useQuery({
+  queryKey: ['comments'],
+  queryFn: () =>
+    fetchData('http://localhost:3000/api/data/comments?timeout=2000').then(
+      (data) => data.comments
+    ),
+});
+
+const comments = ref<Comment[]>([]);
+watch(commentsData, (newComments: Comment[] | undefined) => {
+  if (newComments && Array.isArray(newComments)) {
+    comments.value = newComments;
+  }
+});
+
+provide('comments', comments);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="max-w-7xl mx-auto">
+    <div class="md:grid md:grid-cols-4 gap-x-8">
+      <div class="col-span-1">
+        <h1 class="text-3xl font-bold">Library</h1>
+        <img
+          class="rounded-xl shadow mb-2"
+          src="./assets/image-v2.png"
+          alt="Bookshelf"
+        />
+        <Comments />
+      </div>
+      <div class="col-span-3">
+        <Authors :authors="authorsData" />
+        <Articles :articles="articlesData" :authors="authorsData" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
