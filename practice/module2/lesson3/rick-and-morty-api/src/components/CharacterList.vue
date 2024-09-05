3889e3e1d58c5c2a06e601d5db0a3da6443ebe82
bronisMateusz <mateusz.bronis99@gmail.com>
2024-09-05 18:01:11 +0200

    refactor: Add CharacterList component for displaying a list of characters

---
+++ practice/module2/lesson3/rick-and-morty-api/src/components/CharacterList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { CharacterListResponse } from '../api/api-client-generated/models/CharacterListResponse';
+import CharacterCard from './CharacterCard.vue';
+
+defineProps<{
+  characters: CharacterListResponse | null;
+}>();
+</script>
+
+<template>
+  <ol v-if="characters">
+    <li v-for="character in characters.results" :key="character.id">
+      <CharacterCard :character="character" />
+    </li>
+  </ol>
+  <p v-else>Loading...</p>
+</template>
+
+<style scoped>
+ol {
+  display: grid;
+  gap: 32px 16px;
+  padding-block: 48px 64px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1280px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
